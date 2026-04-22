7f2501934e16e83d81d861271fc0ce466489c5a1
chai788 <patilchaitra488@gmail.com>
2026-04-23 01:47:08 +0530

    Fix output message in Hello.java

--- Hello.java
+++ Hello.java
@@ -4,6 +4,6 @@ public class Hello {
         int b = 20;
         int sum = a + b;
 
-        System.out.println("Sum of numbers are now = " + sum);
+        System.out.println("Sum of numbers are now see= " + sum);
     }
 }
